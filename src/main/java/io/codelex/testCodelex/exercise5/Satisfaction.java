4d7d043f141bcdb33fd4588a2fa529c72230bcae
Gustavs Petris <gustavs.petris98@gmail.com>
2023-06-14 21:45:47 +0300

    second commit

--- src/main/java/io/codelex/testCodelex/exercise5/Satisfaction.java
+++ src/main/java/io/codelex/testCodelex/exercise5/Satisfaction.java
@@ -7,15 +7,26 @@ import java.util.function.Predicate;
 public class Satisfaction {
     public static void main(String[] args) {
         List<String> names = Arrays.asList("John", "Peter", "Angelina", "Bravo");
-        //double percent = partOf(names, (String name) -> name.length() > 4);
-        //System.out.println(percent); //Should print out 0.75 because 75% of names are longer than 4 letters
+        double percent = partOf(names, (String name) -> name.length() > 4);
+        System.out.println(percent); //Should print out 0.75 because 75% of names are longer than 4 letters
 
         List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6);
-        //double evenPercent = partOf(numbers, (Integer n) -> n % 2 == 1);
-        //System.out.println(evenPercent); //Should print out 0.5 because 50% of numbers are even
+        double evenPercent = partOf(numbers, (Integer n) -> n % 2 == 1);
+        System.out.println(evenPercent); //Should print out 0.5 because 50% of numbers are even
 
     }
 
-    //public static <T> double partOf(){}
+    public static <T> double partOf(List<T> items, Predicate<T> condition) {
+        int satisfiedCount = 0;
+        int totalCount = items.size();
+        for (T item : items) {
+            if (condition.test(item)) {
+                satisfiedCount++;
+            }
+        }
+        double percentage = (double) satisfiedCount / totalCount;
+        return percentage;
+    }
+
 
 }
